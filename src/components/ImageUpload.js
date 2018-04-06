fa3b8c47f18bb61a5da648723df72a3c52683459
Hansol Yoon <35543270+dhsol1126@users.noreply.github.com>
2018-04-06 16:18:26 -0600

    Update ImageUpload.js

--- src/components/ImageUpload.js
+++ src/components/ImageUpload.js
@@ -1,116 +1,101 @@
-import { withRouter } from 'react-router'
-import { connect } from 'react-redux'
-import React, {Component} from 'react';
+var express = require('express');
+var multer = require('multer'),
+bodyParser = require('body-parser'),
+path = require('path');
+var mongoose = require('mongoose');
+var Detail = require('../models/detail');
+/*var upload = multer({ dest: 'uploads/' });*/
+mongoose.connect('mongodb://admin:5828@ds123499.mlab.com:23499/backed_safari_db', { useMongoClient: true });
 
-// semantic-ui
-import { Container, Form, Input, Button, Grid } from 'semantic-ui-react'
 
-// alert
-import Alert from 'react-s-alert';
+var upload = multer({storage: multer.diskStorage({
 
-// API
-import * as MyAPI from '../utils/MyAPI'
-import { LOCAL_STRAGE_KEY } from '../utils/Settings'
+  destination: function (req, file, callback) 
+  { callback(null, '../uploads');},
+  filename: function (req, file, callback) 
+  { callback(null, file.fieldname +'-' + Date.now()+path.extname(file.originalname));}
 
- 
-class ImageUpload extends Component {
+}),
 
-  constructor() {
-   super();
-    this.state = {
-      data_uri: null,
-      processing: false
-    }
+fileFilter: function(req, file, callback) {
+  var ext = path.extname(file.originalname)
+  if (ext !== '.png' && ext !== '.jpg' && ext !== '.gif' && ext !== '.jpeg') {
+    return callback(/*res.end('Only images are allowed')*/ null, false)
   }
-
-  handleSubmit(e)
-  {
-    e.preventDefault();
-    const _this = this;
-
-    this.setState({
-      processing: true
-    });
-
-    //FIXME - Need to add user ID here too
-    const { data_uri, filename, filetype } = this.state
-    const params = {
-      data_uri: data_uri,
-      filename: filename,
-      filtype: filetype
-    }
-
-    // Upload picture
-    MyAPI.upload_image(params)
-    .then((data) =>
-    {
-      return new Promise((resolve, reject) =>
-      {
-        if (data.status !== 'success')
-        {
-          let error_text = 'Error';
-          if (data.detail)
-          {
-            error_text = data.detail
-          }
-          reject(error_text)
-
-        }
-        else
-        {
-          // success
-        }
-      })
-    })
-    .then(() => {
-      // redirect
-      this.props.history.push("/dashboard")
-    })
-    .catch((err) => {
-      console.log("err:", err)
-
-      Alert.error(err, {
-        position: 'top-right',
-        effect: 'slide',
-        timeout: 5000
-      });
-   })
-  }
-
-  render() {
-    let processing;
-  let uploaded;
-
-    if (this.state.uploaded_uri) {
-      uploaded = (
-        <div>
-          <h4>Image uploaded!</h4>
-          <img className='image-preview' src={this.state.data_uri} />
-          <pre className='image-link-box'>{this.state.data_uri}</pre>
-          //<img className='image-preview' src={this.state.uploaded_uri} />
-          //<pre className='image-link-box'>{this.state.uploaded_uri}</pre>
-        </div>
-      );
-    }
-
-    if (this.state.processing) {
-      processing = "Processing image, hang tight";
-    }
-
-    return (
-      <div className='row'>
-        <div className='col-sm-12'>
-          <label>Upload an image</label>
-          <form onSubmit={this.handleSubmit} encType="multipart/form-data">
-            <input type="file" onChange={this.handleFile} />
-            <input disabled={this.state.processing} className='btn btn-primary' type="submit" value="Upload" />
-            {processing}
-          </form>
-          {uploaded}
-        </div>
-      </div>
-    );
-  }
+  callback(null, true)
 }
+});
 
-export default ImageUpload;
+var app = new express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// view engine setup
+app.set('views', path.join(__dirname, '../views'));
+app.set('view engine', 'ejs');
+app.use(express.static('uploads'));
+
+app.get('/', function(req, res){
+  Detail.find({}, function(err,data){
+    if(err){
+      console.log(err);
+    }else{
+      res.render('index',{data:data});
+    }
+  })
+  
+});
+
+app.post('/', upload.any(), function(req,res){
+  
+  console.log("req.body"); //form fields
+  console.log(req.body);
+  console.log("req.file");
+  console.log(req.files); //form files
+  
+  if(!req.body && !req.files){
+    res.json({success: false});
+  } else {    
+    var c;
+    Detail.findOne({},function(err,data){
+      console.log("into detail");
+
+      if (data) {
+        console.log("if");
+        c = data.unique_id + 1;
+      }else{
+        c=1;
+      }
+
+      var detail = new Detail({
+
+        unique_id:c,
+        Name: req.body.title,
+        image1:req.files[0].filename,
+      });
+
+      detail.save(function(err, Person){
+        if(err)
+          console.log(err);
+        else
+          res.redirect('/');
+
+      });
+
+    }).sort({_id: -1}).limit(1);
+
+  }
+});
+
+app.post('/delete',function(req,res){
+
+   Detail.findByIdAndRemove(req.body.prodId,function(err, data) {
+
+    console.log(data);
+
+   })
+  res.redirect('/');
+});
+
+var port = 2030;
+app.listen( port, function(){ console.log('listening on port '+port); } );

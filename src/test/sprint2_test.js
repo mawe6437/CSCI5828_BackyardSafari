832ed53070b191c595ed909546972204daaabad8
hansol <hansol.yoon@colorado.edu>
2018-04-19 18:42:21 -0600

    nothing changed

--- src/test/sprint2_test.js
+++ src/test/sprint2_test.js
@@ -110,30 +110,7 @@ describe('Check CreateAccount', function(){
 //     });
 
 
-// expect(res).to.have.status(200);
-
-// 2.  When a user tries to login, check whether information that a user put is correct or not.
-//   //when
-//   var result1 = validate.email('test@naver.com');
-//   var result2 = validate.email('test_1234@naver.com');
-//   var result3 = validate.email('!@test@naver.com');
-//   var result4 = validate.email('test#naver.com');
-//   var result5 = validate.email('test');
-//   var result6 = validate.email('naver.com');
-//   //then
-//   expect(result1).to.be.true;
-//   expect(result2).to.be.true;
-//   expect(result3).to.be.false;
-//   expect(result4).to.be.false;
-//   expect(result5).to.be.false;
-//   expect(result6).to.be.false;
-
-// 3. Check whether a user puts information correctly to the create login page.
-//     Plus, a user has to put both ID and password:
-//     should.have.property('ID');
-
-// var user = {name: 'Scott'};
-// expect(user).to.have.property('name');
+// expect(res).to.have.status(200)
 
 // 4. Check whether a user can see dashboard page after login.
 
